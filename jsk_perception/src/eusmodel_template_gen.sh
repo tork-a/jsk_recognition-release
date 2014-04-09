26aa2d5a8627f546f6d626381225bc687ccb4c7b
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-04-10 02:17:49 +0900

    Trimmed the branch to only the imagesift sub directory

--- jsk_perception/src/eusmodel_template_gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-cd `rospack find jsk_perception`
-mkdir -p launch
-mkdir -p template
-#export ROS_MASTER_URI=http://localhost:12347
-#roscore -p 12347 & (roseus ./src/eusmodel_template_gen.l && kill -INT $!)
-rosrun roseus roseus ./src/eusmodel_template_gen.l
-(cd sample; rosrun roseus roseus ./pose_detector_auto_gen_sample.l "(progn (generate-launcher) (exit))")
