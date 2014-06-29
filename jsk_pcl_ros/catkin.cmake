b3398f1bfad415a9fa06e20746ffd03ef0d18b5a
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2014-06-29 17:03:47 +0900

    Rebase from 'upstream'

--- jsk_pcl_ros/catkin.cmake
+++ jsk_pcl_ros/catkin.cmake
@@ -16,16 +16,32 @@ if($ENV{ROS_DISTRO} STREQUAL "groovy")
 else()
   set(PCL_MSGS pcl_msgs) ## hydro and later
 endif()
+
+if($ENV{ROS_DISTRO} STREQUAL "groovy")
+  set(ML_CLASSIFIERS )
+else()
+  set(ML_CLASSIFIERS ml_classifiers) ## hydro and later
+endif()
+
 find_package(catkin REQUIRED COMPONENTS dynamic_reconfigure pcl_ros nodelet message_generation genmsg ${PCL_MSGS} sensor_msgs geometry_msgs
-  eigen_conversions tf_conversions tf2_ros tf image_transport nodelet cv_bridge)
+  eigen_conversions tf_conversions tf2_ros tf image_transport nodelet cv_bridge ${ML_CLASSIFIERS} sklearn)
+
+find_package(OpenMP)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
 
 add_message_files(FILES PointsArray.msg ClusterPointIndices.msg Int32Stamped.msg SnapItRequest.msg PolygonArray.msg
   ModelCoefficientsArray.msg
   SlicedPointCloud.msg
   BoundingBox.msg
-  BoundingBoxArray.msg)
+  BoundingBoxArray.msg
+  ColorHistogram.msg
+  ColorHistogramArray.msg)
 add_service_files(FILES SwitchTopic.srv  TransformScreenpoint.srv CheckCircle.srv RobotPickupReleasePoint.srv  TowerPickUp.srv EuclideanSegment.srv TowerRobotMoveCommand.srv SetPointCloud2.srv
-  CallSnapIt.srv CallPolygon.srv)
+  CallSnapIt.srv CallPolygon.srv
+  EnvironmentLock.srv
+  PolygonOnEnvironment.srv)
 
 # generate the dynamic_reconfigure config file
 generate_dynamic_reconfigure_options(
@@ -38,6 +54,9 @@ generate_dynamic_reconfigure_options(
   cfg/MultiPlaneExtraction.cfg
   cfg/NormalEstimationIntegralImage.cfg
   cfg/PlaneRejector.cfg
+  cfg/EnvironmentPlaneModeling.cfg
+  cfg/ColorHistogramMatcher.cfg
+  cfg/GridSampler.cfg
   )
 
 find_package(OpenCV REQUIRED core imgproc)
@@ -61,7 +80,7 @@ macro(jsk_pcl_nodelet _nodelet_cpp _nodelet_class _single_nodelet_exec_name)
     LIBRARY DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION})
 endmacro(jsk_pcl_nodelet _nodelet_cpp _nodelet_class _single_nodelet_exec_name)
 
-add_definitions("-O2")
+add_definitions("-O2 -g")
 
 # pcl_ros::Filter based class is not working...
 # https://github.com/ros-perception/perception_pcl/issues/9
@@ -115,8 +134,29 @@ jsk_pcl_nodelet(src/selected_cluster_publisher_nodelet.cpp
   "jsk_pcl/SelectedClusterPublisher" "selected_cluster_publisher")
 jsk_pcl_nodelet(src/plane_rejector_nodelet.cpp
   "jsk_pcl/PlaneRejector" "plane_rejector")
+jsk_pcl_nodelet(src/static_polygon_array_publisher_nodelet.cpp
+  "jsk_pcl/StaticPolygonArrayPublisher" "static_polygon_array_publisher")
+jsk_pcl_nodelet(src/polygon_array_transformer_nodelet.cpp
+  "jsk_pcl/PolygonArrayTransformer" "polygon_array_transformer_nodelet")
+if(NOT $ENV{ROS_DISTRO} STREQUAL "groovy")
+  jsk_pcl_nodelet(src/colorize_segmented_RF_nodelet.cpp
+    "jsk_pcl/ColorizeRandomForest" "colorize_random_forest_result")
+  jsk_pcl_nodelet(src/colorize_random_points_RF_nodelet.cpp
+    "jsk_pcl/ColorizeMapRandomForest" "colorize_random_foreset_result2")
+endif()
+jsk_pcl_nodelet(src/environment_plane_modeling_nodelet.cpp
+  "jsk_pcl/EnvironmentPlaneModeling" "environment_plane_modeling")
+jsk_pcl_nodelet(src/color_histogram_matcher_nodelet.cpp
+  "jsk_pcl/ColorHistogramMatcher" "color_histogram_matcher")
 
-add_library(jsk_pcl_ros SHARED ${jsk_pcl_nodelet_sources})
+jsk_pcl_nodelet(src/grid_sampler_nodelet.cpp
+  "jsk_pcl/GridSampler" "grid_sampler")
+jsk_pcl_nodelet(src/handle_estimator_nodelet.cpp
+  "jsk_pcl/HandleEstimator" "handle_estimator")
+
+add_library(jsk_pcl_ros SHARED ${jsk_pcl_nodelet_sources}
+  src/grid_index.cpp src/grid_map.cpp src/grid_line.cpp src/geo_util.cpp
+  src/pcl_conversion_util.cpp)
 target_link_libraries(jsk_pcl_ros ${catkin_LIBRARIES} ${pcl_ros_LIBRARIES} ${OpenCV_LIBRARIES})
 add_dependencies(jsk_pcl_ros ${PROJECT_NAME}_gencpp ${PROJECT_NAME}_gencfg)
 
