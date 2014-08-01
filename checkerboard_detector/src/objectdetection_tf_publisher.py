709fd2de0bb26ac04f9dacc4b63d056c0ef5cd80
Kei Okada <kei.okada@gmail.com>
2014-08-01 15:51:03 +0900

    Revert "Trimmed the branch to only the jsk_perception sub directory"
    This reverts commit 60761185b553de8923ea7fe898555d68c750022a.

---
+++ checkerboard_detector/src/objectdetection_tf_publisher.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+import roslib
+roslib.load_manifest("posedetection_msgs")
+roslib.load_manifest("dynamic_tf_publisher")
+from posedetection_msgs.msg import ObjectDetection
+from dynamic_tf_publisher.srv import *
+import tf
+
+def callback(msg):
+    global object_messages
+    object_messages = msg
+
+if __name__== '__main__':
+    global object_messages
+    object_messages = None
+    frame_id = rospy.get_param("~frame_id", "object")
+    rospy.init_node('objectdetection_tf_publisher', anonymous=True)
+    subscriber = rospy.Subscriber("ObjectDetection", ObjectDetection, callback)
+    r = rospy.Rate(100)
+    while True:
+        if object_messages:
+            for detected_object in object_messages.objects:
+                br = tf.TransformBroadcaster()
+                br.sendTransform((detected_object.pose.position.x,
+                                  detected_object.pose.position.y,
+                                  detected_object.pose.position.z),
+                                 (detected_object.pose.orientation.x,
+                                  detected_object.pose.orientation.y,
+                                  detected_object.pose.orientation.z,
+                                  detected_object.pose.orientation.w),
+                                 object_messages.header.stamp,
+                                 frame_id,
+                                 object_messages.header.frame_id)
+        r.sleep()
+
