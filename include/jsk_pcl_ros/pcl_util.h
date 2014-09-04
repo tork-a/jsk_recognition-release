c7df831208fbe3b763aebc46ea2ee5cbacb8d4e6
Kei Okada <kei.okada@gmail.com>
2014-09-04 12:21:53 +0900

    Rebase from 'release/groovy/jsk_pcl_ros'

--- include/jsk_pcl_ros/pcl_util.h
+++ include/jsk_pcl_ros/pcl_util.h
@@ -38,4 +38,76 @@
 
 #include <pcl/point_types.h>
 
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/min.hpp>
+#include <boost/accumulators/statistics/max.hpp>
+#include <boost/accumulators/statistics/variance.hpp>
+#include <boost/accumulators/statistics/count.hpp>
+
+#include <set>
+#include <map>
+
+namespace jsk_pcl_ros
+{
+  std::vector<int> addIndices(const std::vector<int>& a,
+                              const std::vector<int>& b);
+
+  class Counter
+  {
+  public:
+    typedef boost::accumulators::accumulator_set<
+    double,
+    boost::accumulators::stats<boost::accumulators::tag::count,
+                               boost::accumulators::tag::mean,
+                               boost::accumulators::tag::min,
+                               boost::accumulators::tag::max,
+                               boost::accumulators::tag::variance> > Accumulator;
+    virtual void add(double v);
+    virtual double mean();
+    virtual double min();
+    virtual double max();
+    virtual int count();
+    virtual double variance();
+  protected:
+    Accumulator acc_;
+  };
+
+  ////////////////////////////////////////////////////////
+  // Graph utility function
+  ////////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////////////////
+  // buildGroupFromGraphMap (recursive function)
+  //   This function retrieves a directional graph, and build
+  //   a set of the indices where can be arrived from a specified
+  //   vertex.
+  // 
+  //   graph_map := A map representeing edges of the graph.
+  //                The graph is one-directional graph.
+  //                the key means "from vertex" and the value
+  //                means the "to indices" from the key vertex.
+  //   from_index := The index to pay attension
+  //   to_indices := The "to indices" from from_index
+  //   output_set := result
+  ////////////////////////////////////////////////////////
+  void buildGroupFromGraphMap(std::map<int, std::vector<int> > graph_map,
+                              const int from_index,
+                              std::vector<int>& to_indices,
+                              std::set<int>& output_set);
+  
+  template <class T>
+  void addSet(std::set<T>& output,
+              const std::set<T>& new_set)
+  {
+    typedef typename std::set<T> Set;
+    typedef typename Set::iterator Iterator;
+    for (Iterator it = new_set.begin();
+         it != new_set.end();
+         ++it) {
+      output.insert(*it);
+    }
+  }
+}
+
 #endif
