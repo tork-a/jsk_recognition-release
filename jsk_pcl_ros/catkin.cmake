5330775e1aabe1ea256af8cfa04af62b729ca1ce
Kei Okada <kei.okada@gmail.com>
2014-09-13 03:00:34 +0900

    Rebase from 'upstream'

--- jsk_pcl_ros/catkin.cmake
+++ jsk_pcl_ros/catkin.cmake
@@ -60,6 +60,7 @@ add_service_files(FILES SwitchTopic.srv  TransformScreenpoint.srv CheckCircle.sr
 # generate the dynamic_reconfigure config file
 generate_dynamic_reconfigure_options(
   cfg/EuclideanClustering.cfg
+  cfg/ColorizeDistanceFromPlane.cfg
   cfg/HSIColorFilter.cfg
   cfg/RGBColorFilter.cfg
   cfg/ImageRotate.cfg
@@ -74,6 +75,7 @@ generate_dynamic_reconfigure_options(
   cfg/OrganizedEdgeDetector.cfg
   cfg/EdgeDepthRefinement.cfg
   cfg/ParallelEdgeFinder.cfg
+  cfg/EdgebasedCubeFinder.cfg
   )
 
 find_package(OpenCV REQUIRED core imgproc)
@@ -190,6 +192,10 @@ jsk_pcl_nodelet(src/edge_depth_refinement_nodelet.cpp
   "jsk_pcl/EdgeDepthRefinement" "edge_depth_refinement")
 jsk_pcl_nodelet(src/parallel_edge_finder_nodelet.cpp
   "jsk_pcl/ParallelEdgeFinder" "parallel_edge_finder")
+jsk_pcl_nodelet(src/edgebased_cube_finder_nodelet.cpp
+  "jsk_pcl/EdgebasedCubeFinder" "edgebased_cube_finder")
+jsk_pcl_nodelet(src/colorize_distance_from_plane_nodelet.cpp
+  "jsk_pcl/ColorizeDistanceFromPlane" "colorize_distance_from_plane")
 
 add_library(jsk_pcl_ros SHARED ${jsk_pcl_nodelet_sources}
   src/grid_index.cpp src/grid_map.cpp src/grid_line.cpp src/geo_util.cpp
