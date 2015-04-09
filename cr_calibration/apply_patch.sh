701a23014ed8533b5b074d4ce335801c02bd6bba
Kei Okada <kei.okada@gmail.com>
2015-04-09 11:35:04 +0900

    Trimmed the branch to only the checkerboard_detector sub directory

--- cr_calibration/apply_patch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-cd `rospack find camera_calibration`;
-if [ `grep -c rational nodes/cameracalibrator.py` == "0" ]; then
-    patch -p0 < `rospack find cr_calibration`/diamondback_camera_calibration.patch;
-fi
