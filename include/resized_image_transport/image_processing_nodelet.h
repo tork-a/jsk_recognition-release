e93a2e05ce1d2cc131ea994cc088166ea0a826de
Kei Okada <kei.okada@gmail.com>
2015-06-19 13:34:27 +0900

    Rebase from 'rpm/indigo/resized_image_transport'

--- include/resized_image_transport/image_processing_nodelet.h
+++ include/resized_image_transport/image_processing_nodelet.h
@@ -40,13 +40,17 @@ namespace resized_image_transport
 
     ros::Publisher image_pub_;
     ros::Subscriber image_sub_;
-    
+    image_transport::Subscriber image_nonsync_sub_;
+    ros::Subscriber camera_info_sub_;
     ros::Publisher width_scale_pub_;
     ros::Publisher height_scale_pub_;
 
+    sensor_msgs::CameraInfoConstPtr info_msg_;
+    
     double resize_x_, resize_y_;
     int dst_width_, dst_height_;
     int max_queue_size_;
+    bool use_camera_subscriber_;
     bool use_snapshot_;
     bool publish_once_;
     bool use_messages_;
@@ -74,23 +78,23 @@ namespace resized_image_transport
       out_times(boost::circular_buffer<double>(100)),
       in_bytes(boost::circular_buffer<double>(100)),
       out_bytes(boost::circular_buffer<double>(100))
-	{ }
-      ~ImageProcessing() { }
+      { }
+    ~ImageProcessing() { }
     
   protected:
     virtual void process(const sensor_msgs::ImageConstPtr &src_img, const sensor_msgs::CameraInfoConstPtr &src_info,
-			 sensor_msgs::ImagePtr &dst_img, sensor_msgs::CameraInfo &dst_info) = 0;
+                         sensor_msgs::ImagePtr &dst_img, sensor_msgs::CameraInfo &dst_info) = 0;
 
     void snapshot_msg_cb (const std_msgs::EmptyConstPtr msg);
 
     bool snapshot_srv_cb (std_srvs::Empty::Request &req,
-			  std_srvs::Empty::Response &res);
-		    
+                          std_srvs::Empty::Response &res);
+        
     void image_cb(const sensor_msgs::ImageConstPtr &img);
-
-
+    void info_cb(const sensor_msgs::CameraInfoConstPtr &info);
+    void image_nonsync_cb(const sensor_msgs::ImageConstPtr &img);
     void callback(const sensor_msgs::ImageConstPtr &img,
-		  const sensor_msgs::CameraInfoConstPtr &info);
+                  const sensor_msgs::CameraInfoConstPtr &info);
   };
 }
 
