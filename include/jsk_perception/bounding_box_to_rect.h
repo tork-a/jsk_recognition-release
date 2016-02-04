2e0d1e96e5cba90f24662577cb672be5f13f96cc
Kei Okada <kei.okada@gmail.com>
2016-02-04 23:14:10 +0900

    Rebase from 'debian/jade/jsk_perception'

--- include/jsk_perception/bounding_box_to_rect.h
+++ include/jsk_perception/bounding_box_to_rect.h
@@ -2,7 +2,7 @@
 /*********************************************************************
  * Software License Agreement (BSD License)
  *
- *  Copyright (c) 2015, JSK Lab
+ *  Copyright (c) 2016, JSK Lab
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -34,60 +34,52 @@
  *********************************************************************/
 
 
-#ifndef JSK_PERCEPTION_POLYGON_ARRAY_COLOR_LIKELIHOOD_H_
-#define JSK_PERCEPTION_POLYGON_ARRAY_COLOR_LIKELIHOOD_H_
+#ifndef JSK_PERCEPTION_BOUNDING_BOX_TO_RECT_H_
+#define JSK_PERCEPTION_BOUNDING_BOX_TO_RECT_H_
 
+#include <jsk_recognition_msgs/BoundingBoxArray.h>
+#include <jsk_recognition_msgs/RectArray.h>
+#include <jsk_recognition_msgs/BoundingBoxArrayWithCameraInfo.h>
+#include <sensor_msgs/CameraInfo.h>
 #include <jsk_topic_tools/diagnostic_nodelet.h>
-#include <jsk_recognition_msgs/PolygonArray.h>
-#include <jsk_recognition_msgs/HistogramWithRangeArray.h>
 #include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/exact_time.h>
 #include <message_filters/sync_policies/approximate_time.h>
-#include <jsk_perception/PolygonArrayColorLikelihoodConfig.h>
-#include <dynamic_reconfigure/server.h>
-#include <opencv2/opencv.hpp>
+#include <jsk_recognition_utils/tf_listener_singleton.h>
+#include <tf/message_filter.h>
 
 namespace jsk_perception
 {
-  class PolygonArrayColorLikelihood: public jsk_topic_tools::DiagnosticNodelet
+  class BoundingBoxToRect: public jsk_topic_tools::DiagnosticNodelet
   {
   public:
-    typedef boost::shared_ptr<PolygonArrayColorLikelihood> Ptr;
-    typedef PolygonArrayColorLikelihoodConfig Config;
-    PolygonArrayColorLikelihood(): DiagnosticNodelet("PolygonArrayColorLikelihood") {}
+    typedef boost::shared_ptr<BoundingBoxToRect> Ptr;
     typedef message_filters::sync_policies::ExactTime<
-      jsk_recognition_msgs::PolygonArray,
-      jsk_recognition_msgs::HistogramWithRangeArray > SyncPolicy;
-    typedef message_filters::sync_policies::ApproximateTime<
-      jsk_recognition_msgs::PolygonArray,
-      jsk_recognition_msgs::HistogramWithRangeArray > ApproximateSyncPolicy;
-
+      sensor_msgs::CameraInfo,
+      jsk_recognition_msgs::BoundingBoxArray > SyncPolicy;
+    
+    BoundingBoxToRect(): DiagnosticNodelet("BoundingBoxToRect") {}
   protected:
     virtual void onInit();
     virtual void subscribe();
     virtual void unsubscribe();
-    virtual void referenceCallback(
-      const jsk_recognition_msgs::HistogramWithRange::ConstPtr& ref_msg);
-    virtual void likelihood(
-      const jsk_recognition_msgs::PolygonArray::ConstPtr& polygon_msg,
-      const jsk_recognition_msgs::HistogramWithRangeArray::ConstPtr& histogram_msg);
-    virtual void configCallback(Config &config, uint32_t level);
-    virtual double compareHist(
-      const cv::MatND& ref_hist, const cv::MatND& target_hist);
-    virtual void readReference(const std::string& file);
+    virtual void inputCallback(const sensor_msgs::CameraInfo::ConstPtr& info_msg,
+                               const jsk_recognition_msgs::BoundingBoxArray::ConstPtr& box_msg);
+
+    virtual void internalCallback(const jsk_recognition_msgs::BoundingBoxArrayWithCameraInfo::ConstPtr& msg);
+
     boost::mutex mutex_;
-    ros::Publisher pub_;
-    ros::Subscriber sub_reference_;
-    boost::shared_ptr<dynamic_reconfigure::Server<Config> > srv_;
+    std::string frame_id_;
+    message_filters::Subscriber<sensor_msgs::CameraInfo> sub_info_;
+    message_filters::Subscriber<jsk_recognition_msgs::BoundingBoxArray> sub_box_;
+    message_filters::Subscriber<jsk_recognition_msgs::BoundingBoxArrayWithCameraInfo> sub_box_with_info_;
     boost::shared_ptr<message_filters::Synchronizer<SyncPolicy> > sync_;
-    boost::shared_ptr<message_filters::Synchronizer<ApproximateSyncPolicy> > async_;
-    message_filters::Subscriber<jsk_recognition_msgs::PolygonArray> sub_polygon_;
-    message_filters::Subscriber<jsk_recognition_msgs::HistogramWithRangeArray> sub_histogram_;
-    jsk_recognition_msgs::HistogramWithRange::ConstPtr reference_;
-    bool approximate_sync_;
-    int max_queue_size_;
-    int coefficient_method_;
-    bool reference_from_file_;
+    tf::TransformListener* tf_listener_;
+    int tf_queue_size_;
+    boost::shared_ptr<tf::MessageFilter<jsk_recognition_msgs::BoundingBoxArrayWithCameraInfo> > tf_filter_;
+    ros::Publisher pub_;
+    ros::Publisher pub_internal_;
   private:
     
   };
