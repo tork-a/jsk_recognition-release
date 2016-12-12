e6265be55b1c2608ee41188c47f4b5460ddde508
Kei Okada <kei.okada@gmail.com>
2016-12-12 18:40:45 +0900

    Revert "Trimmed the branch to only the jsk_pcl_ros sub directory"
    This reverts commit 0894134f17c391e6eb91ce0a07e98dd7850f4677.

---
+++ jsk_recognition_utils/python/jsk_recognition_utils/__init__.py
@@ -0,0 +1,6 @@
+import jsk_recognition_utils.chainermodels
+import jsk_recognition_utils.color
+from jsk_recognition_utils.conversations import *  # NOQA
+from jsk_recognition_utils.feature import *  # NOQA
+from jsk_recognition_utils.mask import *  # NOQA
+from jsk_recognition_utils.visualize import *  # NOQA
