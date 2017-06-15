a16579de4a3e9143d009b6c99eeacdac440a21e2
Kei Okada <kei.okada@gmail.com>
2017-06-16 03:34:43 +0900

    Rebase from 'rpm/jade/jsk_pcl_ros'

--- scripts/install_sample_data.py
+++ scripts/install_sample_data.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python
 
-from jsk_data import download_data
+import argparse
+import multiprocessing
+
+import jsk_data
+
+
+def download_data(*args, **kwargs):
+    p = multiprocessing.Process(
+            target=jsk_data.download_data,
+            args=args,
+            kwargs=kwargs)
+    p.start()
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', dest='quiet', action='store_false')
+    args = parser.parse_args()
+    quiet = args.quiet
+
     PKG = 'jsk_pcl_ros'
 
     download_data(
@@ -51,6 +67,24 @@ def main():
             'sample/data/2016-07-06-12-16-43-person-in-point-cloud/vision.compressed.bag',
         ],
     )
+
+    download_data(
+        pkg_name=PKG,
+        path='sample/data/stereo_arc2017_objects.bag',
+        url='https://drive.google.com/uc?id=0B9P1L--7Wd2va1ZvWHJRT3Y5dkk',
+        md5='4d8f51d0827f66afe5b91473aeaf9c97',
+    )
+
+    download_data(
+        pkg_name=PKG,
+        path='sample/data/sample_add_color_from_image_20170319.bag',
+        url='https://drive.google.com/uc?id=0B9P1L--7Wd2vN284RHBXT1duUWM',
+        md5='d432e7eec587708849ac85fdb3c6247f',
+        compressed_bags=[
+            'sample/data/sample_add_color_from_image_20170319.bag',
+        ],
+    )
+
 
 if __name__ == '__main__':
     main()
