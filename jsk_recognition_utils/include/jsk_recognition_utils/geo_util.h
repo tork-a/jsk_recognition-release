db3621c0dc5fd0d0cc23acbad435ef7bb6bb8d13
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2023-11-10 18:08:44 +0900

    Rebase from 'upstream'

--- jsk_recognition_utils/include/jsk_recognition_utils/geo_util.h
+++ jsk_recognition_utils/include/jsk_recognition_utils/geo_util.h
@@ -13,7 +13,7 @@
  *     notice, this list of conditions and the following disclaimer.
  *   * Redistributions in binary form must reproduce the above
  *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/o2r other materials provided
+ *     disclaimer in the documentation and/or other materials provided
  *     with the distribution.
  *   * Neither the name of the JSK Lab nor the names of its
  *     contributors may be used to endorse or promote products derived
@@ -53,6 +53,7 @@
 ////////////////////////////////////////////////////////
 // PCL headers
 ////////////////////////////////////////////////////////
+#include <pcl/common/common.h>
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl/PointIndices.h>
